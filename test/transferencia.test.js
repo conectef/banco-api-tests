46a105ad88ec2a95ab6ed21b8e3fb2bbce800dc9
Flavio <conectef@gmail.com>
2025-09-19 23:26:15 -0300

    Adicionando fixtures para gerir os corpos da requisições

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -2,6 +2,7 @@ const request = require('supertest');
 const { expect } = require('chai')
 require('dotenv').config()
 const { obterToken } = require('../helpers/autenticacao')
+const postTransferencias = require('../fixtures/postTransferencias.json')
 
 describe('Transferencias', () => {
     describe('POST /transferencias', () => {
@@ -12,31 +13,26 @@ describe('Transferencias', () => {
         })
 
         it('Deve retornar sucesso com 201 quando o valor da transferencia for igual ou acima de R$ 10,00', async () => {
+            const bodyTranferencias = { ...postTransferencias }
+
             const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}`)
-                .send({
-                    contaOrigem: 1,
-                    contaDestino: 2,
-                    valor: 11,
-                    token: ""
-                  })
+                .send(bodyTranferencias)
 
                   expect(resposta.status).to.equal(201);
         })
 
         it('Deve retornar falha com 422 quando o valor da transferencia for abaixo de R$ 10.00', async () => {
+            const bodyTranferencias = { ...postTransferencias }
+            bodyTranferencias.valor = 7
+
             const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}`)
-                .send({
-                    contaOrigem: 1,
-                    contaDestino: 2,
-                    valor: 9,
-                    token: ""
-                  })
+                .send(bodyTranferencias)
 
                   expect(resposta.status).to.equal(422);
         })
